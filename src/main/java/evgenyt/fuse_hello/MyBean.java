0a33c658d3a7331ae1bc78f11f7cddeaeadac83f
EvgenyTyurin <beshere@yandex.ru>
2019-09-18 09:33:42 +0300

    Tika processing

--- src/main/java/evgenyt/fuse_hello/MyBean.java
+++ src/main/java/evgenyt/fuse_hello/MyBean.java
@@ -1,10 +1,25 @@
 package evgenyt.fuse_hello;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.apache.tika.Tika;
+
+/**
+ * File processor bean
+ * @author tey
+ */
+
 public class MyBean {
 	
+	// Process file body using Tika
 	public String process(String msg) {
-		System.out.println("wow " + msg);
+		Tika tika = new Tika();
+		try {
+			InputStream stream = new ByteArrayInputStream(msg.getBytes());
+			msg = tika.parseToString(stream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return msg;
 	}
-
 }
